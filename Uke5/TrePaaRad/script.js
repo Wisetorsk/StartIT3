05c5655e289d97bebbb5a08d9e98df0750f21723
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-15 21:08:21 +0200

    Added more game logic

--- Uke5/TrePaaRad/script.js
+++ Uke5/TrePaaRad/script.js
@@ -1,6 +1,5 @@
 var board = document.getElementById('gameBoard');
 var field = board.getElementsByTagName('div');
-console.log(field);
 
 function check(element) {
     var field2 = [];
@@ -13,15 +12,62 @@ function check(element) {
                 field2.push(field[i]);
             }
         }
+        completeCheck();
         var selected = Math.floor(Math.random() * field2.length);
         if (field2.length > 0) {
             field2[selected].innerHTML = 'O';
             field2[selected].style.color = 'blue';
             } else { console.log('Game is over');
         }
-
+        completeCheck();
 
     } else {
         console.log('occupied');
+    }
+}
+
+function completeCheck() {
+    var corr = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [6, 4, 2],];
+
+    var list = [[], [], [], [], [], [], [], [], ];
+
+    var currentState = document.getElementById('gameBoard').getElementsByTagName('div');
+
+    for (var solution = 0; solution < corr.length; solution++) {
+        for (var index = 0; index < 3; index++) {
+            list[solution][index] = currentState[corr[solution][index]].innerHTML;
+        }
+    }
+
+    for (var x = 0; x < 8; x++) {
+        xlist = '';
+        for (var y = 0; y < 3; y++) {
+            xlist += list[x][y];
+        }
+        if (xlist === 'XXX' || xlist === 'OOO') {
+            console.log('Done');
+            if (xlist === 'XXX') {
+                document.getElementById('result').innerHTML = 'YOU WIN!';
+                lockCells();
+            } else {
+                document.getElementById('result').innerHTML = 'YOU LOSE!';
+                lockCells();
+            }
+        } 
+    }
+    return false;
+}
+
+function lockCells() {
+    for (var x = 0; x < field.length; x++) {
+        field[x].onclick = null;
     }
 }
