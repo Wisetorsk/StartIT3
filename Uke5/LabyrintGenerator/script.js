63fefa3c798e977325144ba5f0b1acf47f91ec24
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-16 13:32:20 +0200

    eastereggstuffs

--- Uke5/LabyrintGenerator/script.js
+++ Uke5/LabyrintGenerator/script.js
@@ -1,4 +1,6 @@
 // JavaScript source code
+var lab;
+
 class Labyrinth {
     constructor(x, y) {
         /*
@@ -6,10 +8,15 @@ class Labyrinth {
          * */
         this.x = x;
         this.y = y;
+        this.maze = [];
+
         this.canvas = document.getElementById('canvas');
         this.canvas.innerHTML = '';
         this.html = '';
         this.draw();
+        this.loadMaze();
+        console.log(this.maze);
+        //this.randomize(0.45);
     }
 
     draw() {
@@ -26,10 +33,10 @@ class Labyrinth {
         console.log('DRAW ROOMS');
         this.html += '<tr>';
         for (var i = 1; i < this.x; i++) {
-            this.html += '<td class="wall high" onclick="noWall(this)"></td >';
+            this.html += '<td class="wall high" ondragEnter="noWall(this)" onclick="noWall(this)"></td >';
             this.html += '<td class="room"></td>';
         }
-        this.html += '<td class="wall high" onclick="noWall(this)"></td >';
+        this.html += '<td class="wall high" ondragEnter="noWall(this)" onclick="noWall(this)"></td >';
         this.html += '</tr>';
     }
 
@@ -38,26 +45,50 @@ class Labyrinth {
         this.html += '<tr>';
         for (var i = 1; i < this.x; i++) {
             this.html += '<td class="wall small"></td>';
-            this.html += '<td class="wall wide" onclick="noWall(this)"></td>'
+            this.html += '<td class="wall wide" ondragEnter="noWall(this)" onclick="noWall(this)"></td>'
         }
         this.html += '<td class="wall small"></td>';
         this.html += '</tr>';
     }
+
+    randomize(chance=.5) {
+        for (var index = 0; index < this.maze.length; index++) {
+            if (Math.random() > chance) {
+                noWall(this.maze[index]);
+            }
+        }
+    }
+
+    loadMaze() {
+        this.maze = document.getElementsByTagName('td');
+        }
 }
 
 
 function noWall(element) {
-    if (element.classList.contains("wall")) {
-        element.classList.remove("wall");
-        element.classList.add("noWall");
-    } else {
-        element.classList.remove("noWall");
-        element.classList.add("wall");
+    if (!element.classList.contains("room") && !element.classList.contains("small")) {
+        if (element.classList.contains("wall")) {
+            element.classList.remove("wall");
+            element.classList.add("noWall");
+        } else if (element.classList.contains("noWall")) {
+            element.classList.remove("noWall");
+            element.classList.add("wall");
+        } else {
+            console.log('ROOM or Node');
+        }
     }
-    
 }
+    
 
 function main() {
-    var labyrinth = new Labyrinth(10, 10);
+    lab = new Labyrinth(10, 10);
 
+}
+
+function party() {
+    lab.randomize(document.getElementById('randomness').value/1000);
+}
+
+function terje() {
+    setInterval(party, 100);
 }
