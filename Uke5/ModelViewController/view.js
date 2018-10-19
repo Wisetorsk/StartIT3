75f3b0363b969642bd11514fea687cbb04a69442
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-19 14:00:04 +0200

    .

--- Uke5/ModelViewController/view.js
+++ Uke5/ModelViewController/view.js
@@ -119,10 +119,9 @@ class UI {
             for (var index in closeIndexes) { //Recursive portion? 
                 var selected = document.getElementById('x' + closeIndexes[index].xClose + 'y' + closeIndexes[index].yClose);
                 if (selected.classList.contains("notOpen") && !selected.classList.contains("isFlag") && !selected.classList.contains("isBomb")) {
-                    selected.click();
                     selected.classList.add("isOpen");
                     selected.classList.remove("notOpen");
-                    
+                    selected.click();
                 }
              }
                 
