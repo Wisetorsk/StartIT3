37074e58354de2132b6f39a903ebd420abd4ac06
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-25 11:12:12 +0200

    added 2nd dimention movement

--- Uke6/Physics/Bounce/physics.js
+++ Uke6/Physics/Bounce/physics.js
@@ -8,8 +8,8 @@ class Ball {
         this.radius = radius;
         this.x = xInit;
         this.y = yInit;
-        this.xVelocity = 0;
-        this.yVelocity = 0;
+        this.xVelocity = 50;
+        this.yVelocity = -50;
     }
 
     updatePosition(gravity) {
@@ -18,9 +18,11 @@ class Ball {
         this.y += this.yVelocity;
         if (this.x >= this.width || this.x < 0) { // Edge impact
             this.xVelocity = -this.xVelocity;
+            console.log("reverse x");
         }
         if (this.y >= this.height || this.y < 0) { // Edge impact
             this.yVelocity = -this.yVelocity;
+            console.log("reverse y");
         }
     }
 }
@@ -31,7 +33,7 @@ class System {
         this.width = width;
         this.height = height;
         this.ball = new Ball(mass, 150, 0, radius, height, width);
-        this.interval = setInterval(updateSimulation, 100);
+        this.interval = setInterval(updateSimulation, 20);
     }
 
     update(ctx) {
