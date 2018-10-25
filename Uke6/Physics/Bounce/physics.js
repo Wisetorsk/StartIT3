2431ba773b916cf8082ffd202b3ff0b4cd075dbc
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-25 09:05:01 +0200

    Further update

--- Uke6/Physics/Bounce/physics.js
+++ Uke6/Physics/Bounce/physics.js
@@ -1,9 +1,11 @@
 // JavaScript source code
+let palette;
 class Ball {
-    constructor(mass, xInit, yInit, height, width) {
+    constructor(mass, xInit, yInit, radius, height, width) {
         this.mass = mass;
         this.height = height;
         this.width = width;
+        this.radius = radius;
         this.x = xInit;
         this.y = yInit;
         this.xVelocity = 0;
@@ -12,7 +14,7 @@ class Ball {
 
     updatePosition(gravity) {
         this.x += this.xVelocity;
-        this.xVelocity += gravity;
+        this.yVelocity += gravity;
         this.y += this.yVelocity;
         if (this.x >= this.width || this.x < 0) { // Edge impact
             this.xVelocity = -this.xVelocity;
@@ -24,16 +26,23 @@ class Ball {
 }
 
 class System {
-    constructor(gravity, mass, height, width) {
+    constructor(gravity, mass, radius, height, width) {
         this.gravity = gravity;
         this.width = width;
         this.height = height;
-        this.ball = new Ball(mass, width / 2, 50, height, width);
+        this.ball = new Ball(mass, 150, 0, radius, height, width);
+        this.interval = setInterval(updateSimulation, 100);
     }
 
-    step() {
+    update(ctx) {
+        ctx.clearRect(0, 0, this.width, this.height);
         this.ball.updatePosition(this.gravity);
+        ctx.beginPath();
+        ctx.arc(this.ball.x, this.ball.y, this.ball.radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "red";
+        ctx.fill();
     }
+
 }
 
 class Palette {
@@ -41,5 +50,18 @@ class Palette {
         this.id = canvasID;
         this.ctx = document.getElementById(this.id).getContext("2d");
         this.ctx.fillStyle = "#FF0000";
+        this.system = new System(9.81, 10, 10, 600, 600);
     }
+
+    oneFrame() {
+        this.system.update(this.ctx);
+    }
+}
+
+updateSimulation = function () {
+    palette.oneFrame();
+}
+
+function main() {
+    palette = new Palette("canvas");
 }
