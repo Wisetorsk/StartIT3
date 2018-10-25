993c2780468616c18475928c737713ced0d252d0
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-25 15:23:11 +0200

    upload

---
+++ Uke6/Physics/DoublePendulum/pendulum.js
@@ -0,0 +1,21 @@
+// JavaScript source code
+class Pendulum {
+    constructor(xAnchor, yAnchor, length) {
+        this.xAnchor = xAnchor;
+        this.yAnchor = yAnchor;
+        this.length = length;
+        this.x = 0;
+        this.y = 0;
+    }
+}
+
+class System {
+    constructor(gravity, animationSteps) {
+        this.gravity = gravity;
+        this.animationSteps = animationSteps;
+    }
+}
+
+function main() {
+
+}
