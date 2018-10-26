155e8a3f2a59614209055c414f87ee2c000522a8
Marius S Kristiansen <mariussk@getacademy.no>
2018-10-26 10:56:42 +0200

    bhjuycfgtv

--- Uke6/Physics/DoublePendulum/pendulum.js
+++ Uke6/Physics/DoublePendulum/pendulum.js
@@ -1,21 +1,73 @@
 // JavaScript source code
+let system, view;
+
 class Pendulum {
-    constructor(xAnchor, yAnchor, length) {
-        this.xAnchor = xAnchor;
-        this.yAnchor = yAnchor;
-        this.length = length;
-        this.x = 0;
+    constructor(radius, theta, width, mass) {
+        this.radius = radius;
+        this.mass = mass;
+        this.yVelocity = 0;
+        this.xVelocity = 0;
+        this.yAcc = 0;
+        this.xAcc = 0;
+        this.theta = theta;
+        this.thetaV = 0;
+        this.thetaA = 0;
+        this.x = width / 2;
         this.y = 0;
     }
+
+    calculate() {
+        this.theta += this.thetaV * 0.05;
+        this.thetaV += this.thetaA * 0.05;
+        this.x = this.radius * Math.sin(this.theta);
+        this.y = this.radius * Math.cos(this.theta);
+    }
+
 }
 
 class System {
-    constructor(gravity, animationSteps) {
+    constructor(gravity, width, height) {
         this.gravity = gravity;
-        this.animationSteps = animationSteps;
+        this.width = width;
+        this.height = height;
+        this.x = width/2;
+        this.y = 0;
+        this.radius = 100;
+    }
+
+    updatePosition() {
+        this.updateTheta();
+    }
+
+    updateTheta() {
+
+    }
     }
 }
 
+class View {
+    constructor(canvas) {
+        this.id = canvas;
+        this.ctx = document.getElementById(this.id).getContext("2d");
+        this.ctx.fillStyle = "#FF0000";
+        this.ctx.strokeStyle = "#FFFFFF";
+    }
+
+    update(system) {
+        system.updatePosition();
+        for (var element in system.elements) {
+            this.ctx.fillRect(system.elements[element].x, system.elements[element].y, 20, 20);
+        }
+    }
+}
+
+
 function main() {
 
+        system.updatePosition();
+        view.update(system);
 }
+
+system = new System(2, 9.81, 100, 600, 600);
+view = new View("viewCanvas");
+system.loadPoints();
