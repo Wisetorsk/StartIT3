79712a58b2bda9742d6852773ef9e29cc211e845
Marius S Kristiansen <mariussk@getacademy.no>
2018-11-15 13:13:31 +0100

    .

--- Uke9/VUE/testproject/src/main.js
+++ Uke9/VUE/testproject/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Dette er en melding'
+    },
+    render: h => h(App)
+})
+
