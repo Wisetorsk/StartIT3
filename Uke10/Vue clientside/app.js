e4eef5b96c70a60260afc49b5028438c0d2e7e25
Marius S Kristiansen <mariussk@getacademy.no>
2018-11-20 14:23:47 +0100

    .

---
+++ Uke10/Vue clientside/app.js
@@ -0,0 +1,59 @@
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            count: 0
+        }
+    },
+    template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+});
+
+new Vue({
+    el: '.app',
+    data: {
+        message: 'Hei, dette er en default message',
+        count: 0
+    },
+    methods: {
+        welcome: function () {
+            console.log(new Date().toLocaleTimeString());
+            this.count++;
+        }
+    },
+    filters: {
+        uppercase: function (text) {
+            return text.toUpperCase();
+        },
+        split: function (text) {
+            return text.split("").join().replace(/,/g, ' ');
+        },
+        lowercase: function (text) {
+            return text.toLowerCase();
+        }
+    },
+    computed: {
+        square: function () {
+            return this.count ** 2;
+        },
+        double: function () {
+            return this.count * 2;
+        },
+        baseTwo: function () {
+            return 2 ** this.count;
+        }
+    }
+});
+
+new Vue({
+    el: '.app2'
+});
+
+
+new Vue({
+    el: '#app3',
+    data: {
+        classInfo: 'blue'
+    },
+    methods: {
+
+    }
+})
