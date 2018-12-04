ebea2a1d8528d114496166921cb8b7994f987d40
Marius S Kristiansen <mariussk@getacademy.no>
2018-12-04 14:17:51 +0100

    Update player.js

--- SPILL/Battleship/player.js
+++ SPILL/Battleship/player.js
@@ -1,7 +1,18 @@
 class Player {
     constructor() {
         if (debug) console.log("User Player instance");
-        this.ships;
+        this.ships = [ // Attribute cells contains the cells that the current ship occupies
+            {type: 'Destroyer', cells: []},
+            {type: 'Destroyer', cells: []},
+            {type: 'Destroyer', cells: []},
+            {type: 'Destroyer', cells: []},
+            {type: 'Submarine', cells: []},
+            {type: 'Submarine', cells: []},
+            {type: 'Submarine', cells: []},
+            {type: 'Battleship', cells: []},
+            {type: 'Battleship', cells: []},
+            {type: 'Carrier', cells: []}
+        ];
         this.shipLengths = [
             { name: 'Destroyer', ships: 4, length: 2 },
             { name: 'Submarine', ships: 3, length: 3 },
@@ -18,18 +29,28 @@ class Player {
         if (debug) console.log("User ship placement method");
         let placed = 0;
         console.log(this.placements);
+        /*
         document.getElementById('playfield').addEventListener('click', function () {
             /* Place ship 
              * Read lastX and lastY and control if they are free
-             */
+             *//*
             console.log(lastCell);
             
             
         }, true);
-        //while (!this.placements === placed) {
-            /* Gotta place'em all! */
-        //}
-        document.getElementById("playfield").removeEventListener("click", function () { });
+        */
+        while (this.placements !== placed) {
+            /* Gotta place'em all! 
+            * Await a change in variable lastCell
+            * */
+            console.log('Ship Placement');
+            var successfull = new Promise(function(resolve, reject) { // Set a promise to await user input
+                resolve(''); // Resolve when a ship is placed properly
+            });
+
+            if (successfull) placed++;
+        }
+        //document.getElementById("playfield").removeEventListener("click", function () { });
     }
 }
 
