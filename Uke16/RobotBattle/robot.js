022451b8a6d0622593fa18be1ea736fa8e40a71c
Marius S Kristiansen <mariussk@getacademy.no>
2019-01-17 10:13:20 +0100

    .

---
+++ Uke16/RobotBattle/robot.js
@@ -0,0 +1,13 @@
+class Robot {
+    constructor(name, orientation = { x: 1, y: 0 }) {
+        this.name = name;
+        this.direction = orientation;
+    }
+
+    action() {
+        return {
+            walk: true,
+            turn: 
+        };
+    }
+}
